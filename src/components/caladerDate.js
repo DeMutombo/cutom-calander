831bac798f650ee10bf6a3c2fc3a9edbf3476bb7
Mutombo <mgtshomba@gmail.com>
2020-12-02 11:08:30 +0200

    Single date style and display

--- src/components/caladerDate.js
+++ src/components/caladerDate.js
@@ -3,12 +3,20 @@ import '../styles/CalanderDate.css'
 
 function  CaladerDate ({day, id, toggleModalState, data}) {
     // const [appointmentTime , setAppTimes ] = useState('')
+    // const [numOfAppointments, setNumOfAppointments] = useState()
     
     return (
         <div  className="CalanderDate" onClick={()=>toggleModalState(id)}>  
         <div className="bg">{day}</div>          
-        {data.map((appT, index) =>  (appT.dayId === id) ? <div key={index}><p className="info">Patient Name: {appT.name}</p><p className="info">App Time:{appT.time}</p></div>: '')}
-    
+        {data.map((appT, index) =>  (appT.dayId === id) 
+                ?   <div className="single" key={index}>
+                      <p className="info">Name: {appT.name}</p>
+                      <p className="info">Time:{appT.time}</p>
+                      {/* {setNumOfAppointments(numOfAppointments + 1)} */}
+                    </div>
+                : ''
+            )}
+
         </div>
     )
 }
